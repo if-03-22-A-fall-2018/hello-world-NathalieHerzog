f054e3fe8379ddfd7a4d01ff729e9471cb593839
NathalieHerzog <43369868+NathalieHerzog@users.noreply.github.com>
2018-09-18 10:53:54 +0200

    Add files via upload

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,20 @@
+
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2AHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 1
+ * Title:			  Procedural Programming
+ * Author:			Nathalie Herzog
+ * ----------------------------------------------------------
+ * Description:
+ * Getting familiar with a first C program and the tool
+ * chain how to build it.
+ * ----------------------------------------------------------
+*/
+
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-  printf("Hello world\n");
+printf("Hello, Nathalie Herzog!\n");
   return 0;
 }
